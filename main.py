13bc75a9762c7d3a6ceb533743ec448d525dd429
jmccallister93 <jaredmccallister93@gmail.com>
2022-05-22 15:16:48 -0700

    Final game added sounds

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter.tix import MAX
 import pygame
 import os
 pygame.font.init()
+pygame.mixer.init()
 
 #Set global width and height for window
 WIDTH, HEIGHT = 900, 500
@@ -16,6 +17,9 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 #set center border
 BORDER = pygame.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)
+#Set sound Glboals
+BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Grenade+1.mp3'))
+BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Gun+Silencer.mp3'))
 #set fonts
 HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
 WINNER_FONT = pygame.font.SysFont('comicsans', 100)
@@ -107,16 +111,17 @@ def handle_bullets(yellow_bullets, red_bullets, yellow, red):
 #Draw winner font
 def draw_winner(text):
     draw_text = WINNER_FONT.render(text, 1, WHITE)
-    WIN.blit(draw_text, (WIDTH/2- draw_text.get_widht()/2, HEIGHT/2 - draw_text.get_height()/2))
+    WIN.blit(draw_text, (WIDTH/2- draw_text.get_width()/2, HEIGHT/2 - draw_text.get_height()/2))
     pygame.display.update()
     pygame.time.delay(5000)
 
+#Main function
 def main():
     #turn the spaceships into pygame rectangle objects
     red = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
     yellow = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
 
-    #Set bullets lsit
+    #Set bullets lsitmain
     red_bullets = []
     yellow_bullets = []
 
@@ -135,22 +140,28 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
 
-            #Fire bullet
+            #Fire bullet, assign ctrl btn add sound
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5)
                     yellow_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()
 
                 if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(red.x, red.y + red.height//2 - 2, 10, 5)
                     red_bullets.append(bullet)
+                    BULLET_FIRE_SOUND.play()
 
             #SEt health based on hits
             if event.type == RED_HIT:
                 red_health -= 1
+                BULLET_HIT_SOUND.play()
+
             if event.type == YELLOW_HIT:
                 yellow_health -=1
+                BULLET_HIT_SOUND.play()
 
                 
         #Set win conditions
@@ -176,7 +187,7 @@ def main():
         #Call window function        
         draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_health)
 
-    pygame.quit()
+    main()
 
 if __name__ == "__main__":
     main()
